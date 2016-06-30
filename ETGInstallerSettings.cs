c9e1e61edecc7c848b861a58acb1bc20460ec2a4
Maik Macho <nuda1998@gmail.com>
2016-06-30 21:01:02 +0200

    Fix ".exe" backup path on Linux

--- ETGInstallerSettings.cs
+++ ETGInstallerSettings.cs
@@ -13,14 +13,15 @@ namespace ETGModInstaller {
 
         public static string ConfigurationPath {
             get {
-                string dir = null;
                 string os = ETGFinder.GetPlatform().ToString().ToLower();
                 if (os.Contains("win")) {
-                    dir = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-                } else if (os.Contains("mac") || os.Contains("osx") || os.Contains("lin") || os.Contains("unix")) {
-                    dir = Environment.GetEnvironmentVariable("HOME");
+                    return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "etgmodconfig.txt");
+                } else if (os.Contains("mac") || os.Contains("osx")) {
+                    return Path.Combine(Environment.GetEnvironmentVariable("HOME"), ".etgmodconfig.txt");
+                } else if (os.Contains("lin") || os.Contains("unix")) {
+                    return Path.Combine(Environment.GetEnvironmentVariable("XDG_CONFIG_HOME"), "", "etgmodconfig.txt");
                 }
-                return Path.Combine(dir, ".etgmodconfig.txt");
+                return Path.Combine(".", "etgmodconfig.txt");
             }
         }
 
